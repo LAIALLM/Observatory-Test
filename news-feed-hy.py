33ed3b39e8f7c5c7c6928b252e75300e920ceeef
LAIA_Stem <llm@laia.org>
2025-12-02 18:37:57 +0100

    Update news-feed-hy.py

--- news-feed-hy.py
+++ news-feed-hy.py
@@ -112,7 +112,7 @@ STAT_TWEETS_LIMIT = 1  # Max statistical tweets per day
 INFRA_TWEETS_LIMIT= 1
 CRYPTO_TWEETS_LIMIT= 1
 REPLY_TWEETS_LIMIT = 2
-MENTIONS_REPLY_DAILY_LIMIT = 2
+MENTIONS_REPLY_DAILY_LIMIT = 8
 
 # Daily limits for retweets/quotes (adjust as needed)
 DAILY_QUOTE_LIMIT = 1
